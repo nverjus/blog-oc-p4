a4fd5ccc5316701a21d996c3a9bc4a84733293e5
Nicolas Verjus <nverjus@protonmail.com>
2018-05-03 15:23:44 +0200

    Paginaton done

--- src/Blog/Repository/PostRepository.php
+++ src/Blog/Repository/PostRepository.php
@@ -25,6 +25,45 @@ class PostRepository extends Repository
         return $posts;
     }
 
+    public function findLastX(int $postsPerPage, int $page)
+    {
+        if ($postsPerPage <= 0) {
+            throw new \InvalidArgumentException('The number of Articles must be a positive integer');
+        }
+
+        $offset = ($page -1) * $postsPerPage;
+        $postss = [];
+
+
+        $req = $this->db->prepare('SELECT * FROM Post ORDER BY id DESC LIMIT :postsPerPage OFFSET :ofset');
+        $req->bindValue(':postsPerPage', $postsPerPage, \PDO::PARAM_INT);
+        $req->bindValue(':ofset', $offset, \PDO::PARAM_INT);
+        $req->execute();
+
+        while ($row = $req->fetch()) {
+            $posts[] = new Post($row);
+        }
+
+        return $posts;
+    }
+
+    public function getNbPages(int $postsPerPage)
+    {
+        $sql = 'SELECT COUNT(id) AS nbPosts FROM Post';
+
+        $req = $this->db->query($sql);
+        $data = $req->fetch();
+        $nbPosts = $data['nbPosts'];
+
+        $nbPages = (int) $nbPosts/$postsPerPage;
+        $nbPages = (int) $nbPages;
+        if ($nbPosts%$postsPerPage != 0) {
+            $nbPages++;
+        }
+
+        return $nbPages;
+    }
+
     public function findById(int $id)
     {
         if ($id <= 0) {
